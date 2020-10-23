98521483d86b54ac7673a68f39924a990afa7803
Piotr Kosiński <piotrkosinski@gmail.com>
2020-10-23 14:13:22 +0200

    feat(): reads adapter

--- packages/backend/src/adapter.ts
+++ packages/backend/src/adapter.ts
@@ -1,161 +1,34 @@
-import moment from "moment";
-import axios from "axios";
-
+import { ReadsService } from "@energyweb/energy-api";
 import {
-  ISmartMeterReadingsAdapter,
-  ISmartMeterRead,
   IDevice,
-  IExternalDeviceId,
+  ISmartMeterRead,
+  ISmartMeterReadingsAdapter,
 } from "@energyweb/origin-backend-core";
-import { BigNumber } from "ethers";
+import moment from "moment";
 
-export class ReaccSmartMeterReadingAdapter
+export class EnergyAPISmartMeterReadingAdapter
   implements ISmartMeterReadingsAdapter {
-  baseUrl = "http://www.reacc.io:3032";
-
-  headers: any = {
-    accept: "application/json",
-  };
-
-  async getLatest(device: IDevice): Promise<ISmartMeterRead> {
-    let realtimeGenerationId: IExternalDeviceId;
-
-    try {
-      realtimeGenerationId = this.getRealtimeGenerationId(device);
-    } catch (e) {
-      console.warn(e);
-      return {
-        meterReading: BigNumber.from(0),
-        timestamp: 0,
-      };
-    }
-
-    const getRequest = await axios.get(
-      `${this.baseUrl}/device/${realtimeGenerationId.id}/energy`,
-      {
-        headers: this.headers,
-        params: {
-          accumulated: true,
-          timeStart: 0,
-          timeEnd: moment().unix(),
-          limit: -1,
-        },
-      }
-    );
-
-    return {
-      meterReading: BigNumber.from(Math.round(getRequest.data[0].energy)),
-      timestamp: moment(getRequest.data[0].measurementTime).unix(),
-    };
-  }
+  constructor(private readonly readsService: ReadsService) {}
 
   async getAll(device: IDevice): Promise<ISmartMeterRead[]> {
-    let realtimeGenerationId: IExternalDeviceId;
+    const start = moment()
+      .subtract(3, "month")
+      .toISOString();
+    const end = moment().toISOString();
 
-    try {
-      realtimeGenerationId = this.getRealtimeGenerationId(device);
-    } catch (e) {
-      console.warn(e);
-      return [];
-    }
+    const res = await this.readsService.find(device.id.toString(), {
+      start,
+      end,
+    });
 
-    const getRequest = await axios.get(
-      `${this.baseUrl}/device/${realtimeGenerationId.id}/energy`,
-      {
-        headers: this.headers,
-        params: {
-          accumulated: false,
-          timeStart: moment()
-            .subtract(1, "month")
-            .endOf("month")
-            .unix(),
-          timeEnd: moment().unix(),
-          limit: -1,
-        },
-      }
-    );
-
-    const smartMeterReadingsByHour: { [key: number]: BigNumber } = {};
-
-    for (const generationData of getRequest.data) {
-      const energy = BigNumber.from(Math.round(generationData.energy));
-
-      if (energy.gt(0)) {
-        const hour =
-          moment(generationData.measurementTime)
-            .endOf("hour")
-            .unix() + 1;
-
-        if (!smartMeterReadingsByHour[hour]) {
-          smartMeterReadingsByHour[hour] = energy;
-        } else {
-          smartMeterReadingsByHour[hour] = smartMeterReadingsByHour[hour].add(
-            energy
-          );
-        }
-      }
-    }
-
-    let currentSmartMeterReading = BigNumber.from(0);
-    const finalSortedAndHourAccumulatedSmReads: ISmartMeterRead[] = [];
-
-    for (const unixTimestamp of Object.keys(smartMeterReadingsByHour)) {
-      currentSmartMeterReading = currentSmartMeterReading.add(
-        smartMeterReadingsByHour[Number(unixTimestamp)]
-      );
-
-      finalSortedAndHourAccumulatedSmReads.push({
-        meterReading: currentSmartMeterReading,
-        timestamp: Number(unixTimestamp),
-      });
-    }
-
-    const latestMeterReading = (await this.getLatest(device)).meterReading;
-
-    // Reacc API only provides us with energy generation, not smart meter readings
-    // We need to offset those reading with the last smart meter reading (from getLatest)
-    // to get the correct smart meter reading values
-    const unShownReadsCompensation = latestMeterReading.sub(
-      finalSortedAndHourAccumulatedSmReads[
-        finalSortedAndHourAccumulatedSmReads.length - 1
-      ].meterReading
-    );
-    const unShownReadsCompensated = finalSortedAndHourAccumulatedSmReads.map(
-      (reading) => {
-        return {
-          meterReading: BigNumber.from(reading.meterReading).add(
-            unShownReadsCompensation
-          ),
-          timestamp: reading.timestamp,
-        };
-      }
-    );
-
-    return unShownReadsCompensated;
+    return res.map((read) => ({
+      meterReading: read.value.toString(),
+      timestamp: read.timestamp.getTime(),
+    }));
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async save(device: IDevice, smRead: ISmartMeterRead[]): Promise<void> {
     return null;
-  }
-
-  private getRealtimeGenerationId(device: IDevice): IExternalDeviceId {
-    const nonExistingIdError = new Error(
-      `Device ${device.facilityName} with id ${device.id} doesn't have a Real-time Generation ID set.`
-    );
-
-    if (!device.externalDeviceIds) {
-      throw nonExistingIdError;
-    }
-
-    const realtimeGenerationId = device.externalDeviceIds.find(
-      (externalDeviceId) => externalDeviceId.type === "Real-time Generation ID"
-    );
-
-    if (!realtimeGenerationId?.id) {
-      throw nonExistingIdError;
-    }
-
-    return realtimeGenerationId;
   }
 }
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,49 +1,53 @@
-import fs from 'fs';
-import { AppModule as ExchangeModule } from '@energyweb/exchange';
-import { LoggerService } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { useContainer } from 'class-validator';
+import fs from "fs";
+import { AppModule as ExchangeModule } from "@energyweb/exchange";
+import { LoggerService } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { useContainer } from "class-validator";
 
-import { ReaccSmartMeterReadingAdapter } from './adapter';
-import { OriginAppModule } from './origin-app.module';
-import { getPort } from './port';
+import { EnergyAPISmartMeterReadingAdapter } from "./adapter";
+import { OriginAppModule } from "./origin-app.module";
+import { getPort } from "./port";
+import { ReadsService } from "@energyweb/energy-api";
+import { ConfigService } from "@nestjs/config";
 
 export async function startAPI(logger?: LoggerService) {
-    const PORT = getPort();
-    const getVersion = () => {
-        let info;
-        if (fs.existsSync(`${__dirname}/../../../package.json`)) {
-            info = fs.readFileSync(`${__dirname}/../../../package.json`);
-        } else {
-            return 'unknown';
-        }
-
-        const parsed = JSON.parse(info.toString());
-
-        return {
-            '@ptt-origin/backend': parsed.version,
-            exchange: parsed.dependencies['@energyweb/exchange'],
-            'origin-backend': parsed.dependencies['@energyweb/origin-backend']
-        };
-    };
-
-    console.log(`Backend starting on port: ${PORT}`);
-    console.log(`Backend versions: ${JSON.stringify(getVersion())}`);
-
-    const adapter = new ReaccSmartMeterReadingAdapter();
-
-    const app = await NestFactory.create(OriginAppModule.register(adapter));
-    app.enableCors();
-    app.setGlobalPrefix('api');
-
-    if (logger) {
-        app.useLogger(logger);
+  const PORT = getPort();
+  const getVersion = () => {
+    let info;
+    if (fs.existsSync(`${__dirname}/../package.json`)) {
+      info = fs.readFileSync(`${__dirname}/../package.json`);
+    } else {
+      return "unknown";
     }
 
-    await app.listen(PORT);
+    const parsed = JSON.parse(info.toString());
 
-    // TODO: this should be OriginAppModule but for some reason it crashes, probably due to the fact it's dynamic module
-    useContainer(app.select(ExchangeModule), { fallbackOnErrors: true });
+    return {
+      "@ptt-origin/backend": parsed.version,
+      exchange: parsed.dependencies["@energyweb/exchange"],
+      "origin-backend": parsed.dependencies["@energyweb/origin-backend"],
+    };
+  };
 
-    return app;
+  console.log(`Backend starting on port: ${PORT}`);
+  console.log(`Backend versions: ${JSON.stringify(getVersion())}`);
+
+  const adapter = new EnergyAPISmartMeterReadingAdapter(
+    new ReadsService(new ConfigService())
+  );
+
+  const app = await NestFactory.create(OriginAppModule.register(adapter));
+  app.enableCors();
+  app.setGlobalPrefix("api");
+
+  if (logger) {
+    app.useLogger(logger);
+  }
+
+  await app.listen(PORT);
+
+  // TODO: this should be OriginAppModule but for some reason it crashes, probably due to the fact it's dynamic module
+  useContainer(app.select(ExchangeModule), { fallbackOnErrors: true });
+
+  return app;
 }
